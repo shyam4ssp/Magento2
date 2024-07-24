f08a039fc435957478f5790ac1d5bf33a99d4fd7
shyam4ssp <shyam4ssp@gmail.com>
2024-07-24 13:04:47 +0530

    Theme for magento 2 admin

---
+++ theme-for-admin/app/design/adminhtml/Dcw/backend/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright (c) 2022.
+ * Author	JQ
+ * Category	backend
+ *
+ *
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'adminhtml/Dcw/backend', // Example: 'adminhtml/Magento/backend'
+    __DIR__
+);
